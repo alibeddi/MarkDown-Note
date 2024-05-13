88529eba8068f0add811327ede16ed621236e430
alibeddi <ali7beddi@gmail.com>
2024-05-13 09:54:48 +0100

    feat(NotesList): add notes list page && create filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,89 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from 'react-router-dom'
 
-import {Container} from "react-bootstrap"
-import { NewNote } from "./NewNote"
-import { useLocalStorage } from "./useLocalStorage"
-import { useMemo } from "react"
-import { v4 as uuidV4 } from "uuid"
+import { Container } from 'react-bootstrap'
+import { NewNote } from './NewNote'
+import { NoteList } from './NoteList'
+import { useLocalStorage } from './useLocalStorage'
+import { useMemo } from 'react'
+import { v4 as uuidV4 } from 'uuid'
 
 export type Note = {
-  id: string
+    id: string
 } & NoteData
 export type RawNote = {
-  id: string
-}& RawNoteData
+    id: string
+} & RawNoteData
 
-  export type RawNoteData = {
+export type RawNoteData = {
     title: string
     markdown: string
     tagIds: string[]
-  }
-
+}
 
 export type NoteData = {
-  title: string
-  markdown: string
-  tags: Tag[]
+    title: string
+    markdown: string
+    tags: Tag[]
 }
- export type Tag = {
+export type Tag = {
     id: string
     label: string
-  }
+}
 
 function App() {
-  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
-  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
+    const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
+    const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
 
-  const notesWithTags = useMemo(() => {
-    return notes.map(note =>{
-    return {
-      ...note, tags: tags.filter(tag => note.tagIds.includes(tag.
-        id))
+    const notesWithTags = useMemo(() => {
+        return notes.map((note) => {
+            return {
+                ...note,
+                tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+            }
+        })
+    }, [notes, tags])
+
+    function onCreateNote({ tags, ...data }: NoteData) {
+        setNotes((prevNotes) => {
+            return [
+                ...prevNotes,
+                { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+            ]
+        })
     }
-    })
-  }, [notes, tags])
 
-  function onCreateNote({ tags, ...data }: NoteData) {
-    setNotes(prevNotes => {
-      return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }]
-    })
-  }
+    function addTag(tag: Tag) {
+        setTags((prevTags) => [...prevTags, tag])
+    }
 
-function addTag(tag: Tag) {
-  setTags(prevTags => [...prevTags, tag])
-}
-   
-  return (
-    <Container className='my-4'>
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
-      <Route path="/:id">
-        <Route index element={<h1>Show</h1>} />
-        <Route path="edit" element={<h1>Edit</h1>} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-    </Container>
-  )
+    return (
+        <Container className="my-4">
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <NoteList notes={notesWithTags} availableTags={tags} />
+                    }
+                />
+                <Route
+                    path="/new"
+                    element={
+                        <NewNote
+                            onSubmit={onCreateNote}
+                            onAddTag={addTag}
+                            availableTags={tags}
+                        />
+                    }
+                />
+                <Route path="/:id">
+                    <Route index element={<h1>Show</h1>} />
+                    <Route path="edit" element={<h1>Edit</h1>} />
+                </Route>
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Container>
+    )
 }
 export default App
