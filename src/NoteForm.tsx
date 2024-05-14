91d53ae4f3e13b8ccf79f38b7b55243f6725e62a
alibeddi <ali7beddi@gmail.com>
2024-05-14 09:34:04 +0100

    feat(edit): create edit markdown

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -10,12 +10,19 @@ type NoteFormProps = {
     onSubmit: (data: NoteData) => void
     onAddTag: (tag: Tag) => void
     availableTags: Tag[]
-}
+} & Partial<NoteData>
 
-export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+export function NoteForm({
+    onSubmit,
+    onAddTag,
+    availableTags,
+    title = '',
+    markdown = '',
+    tags = [],
+}: NoteFormProps) {
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
-    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+    const [selectedTags, setSelectedTags] = useState<Tag[]>(tags)
     const navigate = useNavigate()
 
     function handleSubmit(e: FormEvent) {
@@ -34,7 +41,11 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
                     <Col>
                         <Form.Group controlId="title">
                             <Form.Label>Title</Form.Label>
-                            <Form.Control required ref={titleRef} />
+                            <Form.Control
+                                required
+                                ref={titleRef}
+                                defaultValue={title}
+                            />
                         </Form.Group>
                     </Col>
                     <Col>
@@ -74,6 +85,7 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
                         as="textarea"
                         rows={15}
                         ref={markdownRef}
+                        defaultValue={markdown}
                     />
                 </Form.Group>
 
